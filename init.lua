ccdac595df5c04394edbf0fb454b85dd20b59bb6
Huy Hoang <boydaihungst@gmail.com>
2024-11-30 04:50:46 +0700

    fix: update previewer/preloader/spotter/fetcher arguments

--- init.lua
+++ init.lua
@@ -2,11 +2,11 @@ local M = {}
 local function fail(s, ...)
 	ya.notify({ title = "Torrent preview", content = s:format(...), timeout = 5, level = "error" })
 end
-function M:peek()
+function M:peek(job)
 	-- launch process
 	local process, cmd_err = Command("transmission-show")
 		:args({
-			tostring(self.file.url),
+			tostring(job.file.url),
 		})
 		:stdout(Command.PIPED)
 		:spawn()
@@ -16,7 +16,7 @@ function M:peek()
 		return 0
 	end
 
-	local limit = self.area.h
+	local limit = job.area.h
 	-- read and count lines from process
 	local i, lines = 0, ""
 	repeat
@@ -27,32 +27,32 @@ function M:peek()
 
 		i = i + 1
 		-- only concatenate lines that are past 'skip' number of STDOUT
-		if i > self.skip then
+		if i > job.skip then
 			lines = lines .. next
 		end
-	until i >= self.skip + limit -- until reader reaches max number of lines on screen plus skip
+	until i >= job.skip + limit -- until reader reaches max number of lines on screen plus skip
 
 	process:start_kill()
 
 	-- if paged below all output, run peek again with smaller skip
-	if self.skip > 0 and i < self.skip + limit then
+	if job.skip > 0 and i < job.skip + limit then
 		ya.manager_emit(
 			"peek",
-			{ tostring(math.max(0, i - limit)), only_if = tostring(self.file.url), upper_bound = "" }
+			{ tostring(math.max(0, i - limit)), only_if = tostring(job.file.url), upper_bound = "" }
 		)
 	-- preview torrent
 	else
-		ya.preview_widgets(self, { ui.Text.parse(lines):area(self.area):wrap(ui.Text.WRAP) })
+		ya.preview_widgets(job, { ui.Text.parse(lines):area(job.area):wrap(ui.Text.WRAP) })
 	end
 end
 
-function M:seek(units)
+function M:seek(job)
 	local h = cx.active.current.hovered
-	if h and h.url == self.file.url then
-		local step = math.floor(units * self.area.h / 10)
+	if h and h.url == job.file.url then
+		local step = math.floor(job.units * job.area.h / 10)
 		ya.manager_emit("peek", {
 			math.max(0, cx.active.preview.skip + step),
-			only_if = self.file.url,
+			only_if = job.file.url,
 		})
 	end
 end
